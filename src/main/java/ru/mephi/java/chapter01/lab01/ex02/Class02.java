81e63cd19e233c113f1d9f28037456b3686b2262
godspeeedx <smirnov3g@yandex.ru>
2021-02-14 00:49:59 +0300

    update to the second ex

--- src/main/java/ru/mephi/java/chapter01/lab01/ex02/Class02.java
+++ src/main/java/ru/mephi/java/chapter01/lab01/ex02/Class02.java
@@ -1,7 +1,22 @@
 package ru.mephi.java.chapter01.lab01.ex02;
 
+// Напишите программу, вводящую целочисленное (как положительное, так
+// и отрицательное) значение угла и нормализующую его в пределах от 0
+// до 359 градусов. Попробуйте сделать это сначала с помощью операции %,
+// а затем метода floorMod().
+
+import java.util.Scanner;
+
 public class Class02 {
     public static void main(String[] args) {
-        System.out.println("class name is strange");
+        System.out.println("Введите значение угла:");
+        Scanner in = new Scanner(System.in);
+
+        while (!in.hasNextInt()) {
+            System.out.println("Введите корректное значение!");
+            in.next();
+        }
+        int number = in.nextInt();
+
     }
 }
