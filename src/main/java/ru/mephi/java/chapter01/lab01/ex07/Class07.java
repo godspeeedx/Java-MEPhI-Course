942eca76214ad666fcf69589ef88dbc7261f1514
Tram_ham <57100455+GrigKosilov@users.noreply.github.com>
2021-03-09 11:34:06 +0300

    7 - исправления Олеси
    Логика - Олеся
    Функции ввода и форматирование кода - Гриша

--- src/main/java/ru/mephi/java/chapter01/lab01/ex07/Class07.java
+++ src/main/java/ru/mephi/java/chapter01/lab01/ex07/Class07.java
@@ -6,52 +6,113 @@ package ru.mephi.java.chapter01.lab01.ex07;
 import java.util.Scanner;
 
 public class Class07 {
-
     public static void main(String[] args) {
+        short first = inShort(IntWCheck("Введите большее число", 0, 65535));
+        short second = inShort(IntWCheck("Введите число-делитель", 0, 65535));
 
-        System.out.println("Введите два числа в пределах от 0 до 65535:");
-        Scanner in = new Scanner(System.in);
-
-        int number1 = in.nextInt();
-        int number2 = in.nextInt();
-        short sh1 = ToShort(number1);
-        short sh2 = ToShort(number2);
-
-        /*System.out.printf("1 - %d\n", sh1);
-        System.out.printf("2 - %d\n", sh2);
-        System.out.println(sh1 + sh2);
-        System.out.println(sh1 - sh2);*/
-
-        System.out.printf("\nРезультат сложения - %d\n", ToInt(Addition(sh1, sh2)));
-        System.out.printf("Результат вычитания - %d\n", ToInt(Subtraction(sh1, sh2)));
-        System.out.printf("Результат умножения - %d\n", ToInt(Multiplication(sh1, sh2)));
-        System.out.printf("Результат деления - %d\n", ToInt(Division(sh1, sh2)));
-        System.out.println("\nЗадание выполнено!");
-
+        short sum = summar(first, second);
+        short sub = subst(first, second);
+        short mult = multix(first, second);
+        short div = divide(first, second);
+        short mod = modDividing(first, second);
+        printRes(sum, sub, mult, div, mod);
     }
 
-    public static short ToShort(int number) {
-        return (short) (number - 32768);
+    public static int Int(String text) {
+        Scanner sc = new Scanner(System.in);
+        do {
+            System.out.println(text);
+            if (sc.hasNextInt() == true)
+                break;
+            else {
+                System.out.println("Wrong input type, try from -2 147 483 648 to 2 147 483 647 ");
+                sc.next();
+            }
+        } while (true);
+        int input = sc.nextInt();
+        System.out.println("You've printed: " + input);
+        //sc.close();
+        return input;
     }
 
-    public static int ToInt(short number) {
-        return (number + 32768);
+    public static int IntWCheck(String text, int min, int max) { // -2 147 483 648 < x < +2 147 483 647
+        int input;
+        do {
+            input = Int(text);
+            if (input > min && input < max) break;
+            else System.out.println("Please, try values between " + min + " and " + max);
+        } while (true);
+        return input;
     }
 
-    public static short Addition(short num1, short num2) {
-        return (short) (num1 + num2);
+    public static short inShort(int a) {
+        return (short) (a + Short.MIN_VALUE);
     }
 
-    public static short Subtraction(short num1, short num2) {
-        return (short) (num1 - num2);
+    public static int shortIn(short a) {
+        return a - Short.MIN_VALUE;
     }
 
-    public static short Multiplication(short num1, short num2) {
-        return (short) (num1 * num2);
+    public static short summar(short a, short b) {
+        if ((a + b - Short.MIN_VALUE) < -32768 || (a + b - Short.MIN_VALUE) > 32767) {
+            return -1;
+        } else return (short) (a + b - Short.MIN_VALUE);
     }
 
-    public static short Division(short num1, short num2) {
-        return (short) (num1 / num2);
+    public static short subst(short a, short b) {
+        if ((a - b) < 0 || (a - b) > 65535) return -1;
+        else return (short) ((a - b) + Short.MIN_VALUE);
     }
 
+    public static short multix(short a, short b) {
+        if (((a - Short.MIN_VALUE) * (b - Short.MIN_VALUE)) < 0 || ((a - Short.MIN_VALUE) * (b - Short.MIN_VALUE)) > 65535)
+            return -1;
+        else return (short) (((a - Short.MIN_VALUE) * (b - Short.MIN_VALUE)) + Short.MIN_VALUE);
+    }
+
+    public static short divide(short a, short b) {
+        if (b != Short.MIN_VALUE) {
+            if (((a - Short.MIN_VALUE) / (b - Short.MIN_VALUE)) < 0 || ((a - Short.MIN_VALUE) / (b - Short.MIN_VALUE)) > 65535) {
+                return -1;
+            } else {
+                return (short) ((a - Short.MIN_VALUE) / (b - Short.MIN_VALUE) + Short.MIN_VALUE);
+            }
+        } else return -2;
+    }
+
+    public static short modDividing(short a, short b) {
+        if (b != Short.MIN_VALUE) {
+            if ((a - Short.MIN_VALUE) % (b - Short.MIN_VALUE) < 0 || (a - Short.MIN_VALUE) % (b - Short.MIN_VALUE) > 65535) {
+                return -1;
+            } else {
+                return (short) ((a - Short.MIN_VALUE) % (b - Short.MIN_VALUE) + Short.MIN_VALUE);
+            }
+        } else return -2;
+    }
+
+    public static void printRes(short sum, short sub, short mux, short div, short mod) {
+        if (sum != -1) {
+            System.out.printf("Сумма %d\n", shortIn(sum));
+        } else System.out.println("Значение суммы не лежит в заданном диапазоне");
+
+        if (sub != -1) {
+            System.out.printf("Разность %d\n", shortIn(sub));
+        } else System.out.println("Значение разности не лежит в заданном диапазоне");
+
+        if (mux != -1) {
+            System.out.printf("Произведение %d\n", shortIn(mux));
+        } else System.out.println("Значение умножения не лежит в заданном диапазоне");
+
+        if (div != -1) {
+            if (div != -2) {
+                System.out.printf("Частное %d\n", shortIn(div));
+            } else System.out.println("Деление на ноль");
+        } else System.out.println("Значение деления не лежит в заданном диапазоне");
+
+        if (mod != -1) {
+            if (mod != -2) {
+                System.out.printf("Остаток %d\n", shortIn(mod));
+            } else System.out.println("Деление на ноль");
+        } else System.out.println("Значение остатка не лежит в заданном диапазоне");
+    }
 }
