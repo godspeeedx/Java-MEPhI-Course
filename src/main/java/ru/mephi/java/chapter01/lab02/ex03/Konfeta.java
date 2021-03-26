8cfaff72fb34baa3096bb14051304d63274278f6
Tram_ham <57100455+GrigKosilov@users.noreply.github.com>
2021-03-26 16:37:41 +0300

    Мой 3

--- src/main/java/ru/mephi/java/chapter01/lab02/ex03/Konfeta.java
+++ src/main/java/ru/mephi/java/chapter01/lab02/ex03/Konfeta.java
@@ -1,4 +1,4 @@
-package ru.mephi.java.chapter01.lab02.ex03;
+package ru.mephi.java.ch02.task03;
 
 // Может ли модифицирующий метод вообще возвращать что-нибудь, кроме типа void?
 // Можно ли создать метод доступа с возвращаемым типом void, т.е. ничего фактически не возвращающий?
@@ -6,59 +6,40 @@ package ru.mephi.java.chapter01.lab02.ex03;
 
 
 import java.util.Scanner;
-public class Konfeta {
-
-    private String color;
-    private boolean edible;
+public class Apple {
+    private boolean rotten;
     private int amount;
 
-    public Konfeta() {
-        color = "Green";
-        edible = true;
-        amount = 1;
+    public Apple() {
+        rotten = false;
+        amount = 3;
     }
-
-    public void setColor(String color) { // Мутатор
-        this.color = color;
+    public void setRottenness(boolean rotten) { // Модифиц
+        this.rotten = rotten;
     }
-
-    public int eat(int amount) { //Мутатор, возвращающий int.
+    public int eat(int amount) { //Модиф, возвращающий int.
         this.amount -= amount;
         return amount;
     }
-
-    public boolean getEdible() { //Аксессор
-        return this.edible;
+    public boolean getRottenness() { //Доступ
+        return rotten;
     }
-
-    public void useless(){ //Аксессор, который не изменяет объект и ничего не возвращает.
-        int useless = 2+2;
+    public void printRottenness(){ //Доступ, который не изменяет объект и ничего не возвращает.
+        System.out.println(rotten);
     }
 
     public static void main(String[] args) {
-
-        Scanner in = new Scanner(System.in);
-        System.out.println("Choose the color of the candy!");
-        String color = in.next();
-
-        Konfeta marmeladka = new Konfeta();
-
-        marmeladka.useless();
-
-        marmeladka.setColor(color);
-        System.out.println("Your marmeladka is " + marmeladka.color);
-        if (marmeladka.getEdible())
-            System.out.println("You can eat it.");
-        else
-            System.out.println("You better not eat it!");
-
-        System.out.println("How much do you want to eat?");
-        int hunger = in.nextInt();
-        int eaten = marmeladka.eat(hunger);
-        System.out.println("You have eaten " + eaten + " piece." + " There is " + marmeladka.amount + " pieces left.");
-
-
+        System.out.println("Let me give you some apples");
+        Apple you = new Apple();
+        System.out.println("Now you have " + you.amount + " apples");
+        System.out.println("Let's eat one");
+        you.eat(1);
+        System.out.println("You ate one, and now you have " + you.amount + " apples");
+        System.out.println("Are you sure that it was okey to eat this apples? Am I true that there are rotten?");
+        System.out.println("You are " + you.getRottenness());
+        you.setRottenness(true);
+        System.out.println("And how about now? Am I true? \n You are");
+        you.printRottenness();
     }
 
 }
-
