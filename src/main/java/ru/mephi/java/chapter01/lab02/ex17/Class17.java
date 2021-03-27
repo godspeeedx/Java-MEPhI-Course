0d68547b343c34e8e33b8f7cc559a89badb080c4
Tram_ham <57100455+GrigKosilov@users.noreply.github.com>
2021-03-27 03:52:58 +0300

    Мой 17

--- src/main/java/ru/mephi/java/chapter01/lab02/ex17/Class17.java
+++ src/main/java/ru/mephi/java/chapter01/lab02/ex17/Class17.java
@@ -1,84 +1,82 @@
-package ru.mephi.java.chapter01.lab02.ex17;
+package ru.mephi.java.ch02.task17;
 
 //Предоставьте итератор — объект, извлекающий по порядку элементы очереди из предыдущего упражнения.
 // Сделайте класс Iterator вложенным и определите в нем методы next () и hasNext ().
 // Определите в классе Queue метод iterator (), возвращающий ссылку на объект Queue .Iterator.
 // Должен ли внутренний класс Iterator быть статическим?
 
+public class Queue {
+    //Из 16ого
+    private static class Node {
+        private String data = "";
+        public Node next = null;
 
-public class Class17 {
-
-    static class Queue {
-
-        private Node head;
-        private Node tail;
-
-        private static class Node {
-            private final String data;
-            Node next;
-
-            public Node(String data) {
-                this.data = data;
-            }
-
-            public String getData() {
-                return this.data;
-            }
+        public Node(String data) {
+            this.data = data;
         }
-
-        private class Iterator {
-            private Node current = head;
-
-            public boolean hasNext() {
-                return current != null;
-            }
-
-            public String next() {
-                String result = hasNext() ? current.getData() : null;
-                this.current = hasNext() ? this.current.next : null;
-                return result;
-            }
-        }
-
-
-        public void add(String data) {
-            Node n = new Node(data);
-            if (this.head == null) {
-                this.head = n;
-                this.tail = n;
-            } else {
-                this.tail.next = n;
-                this.tail = n;
-            }
-        }
-
-        public String remove() {
-            if (this.head == null) {
-                return null;
-            } else {
-                Node n = this.head;
-                this.head = this.head.next;
-                return n.getData();
-            }
-        }
-
-        public Iterator iterator() {
-            return new Iterator();
+        public String getData() {
+            return data;
         }
     }
 
+    public Node head=null, tail=null;
+    public void add(String data) {
+        if (tail == null) {
+            head = new Node(data);
+            tail = head;
+        }
+        else {
+            tail.next = new Node(data);
+            tail = tail.next;
+        }
+    }
+    public String remove() {
+        if (head == null)
+            return null;
+
+        Node removedNode = head;
+        head = head.next;
+        return removedNode.getData();
+    }
+    //Из 16ого
+
+    public Iterator iterator() {
+        return new Iterator(head);
+    }
+    public static class Iterator {
+        private Node current;
+
+        public Iterator (Node current){ this.current=current; }
+        public String getCurrentData() {
+            return current.getData();
+        }
+        public boolean hasNext() {
+            return current != null;
+        }
+        public String next() {
+            if (current != null){
+                String dataBuf=getCurrentData();
+                current = current.next;
+                return dataBuf;
+            }
+            return null;
+        }
+    }
+
+}
+
+public class Test {
     public static void main(String[] args) {
         Queue queue = new Queue();
-        queue.add("First");
-        queue.add("Second");
-        queue.add("Third");
-        queue.add("Fourth");
-        queue.add("Fifth");
-        queue.add("Sixth");
-        while (queue.iterator().hasNext()){
-            System.out.println(queue.iterator().next());
-            queue.remove();
-        }
+        for (int i=0; i<6; i++)
+            queue.add("" + i);
+
+        Queue.Iterator pointer = queue.iterator();
+        while (pointer.hasNext())
+            System.out.println(pointer.next());
+
+        for (int i=0; i<8; i++)
+            System.out.println(queue.remove());
 
     }
 }
