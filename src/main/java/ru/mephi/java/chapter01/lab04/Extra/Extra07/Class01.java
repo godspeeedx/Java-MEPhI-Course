8173850d3882dffb268c61535ff54a204890ecc8
godspeeedx <smirnov3g@yandex.ru>
2021-10-16 00:04:14 +0300

    lab4 another commit

--- src/main/java/ru/mephi/java/chapter01/lab04/Extra/Extra07/Class01.java
+++ src/main/java/ru/mephi/java/chapter01/lab04/Extra/Extra07/Class01.java
@@ -6,24 +6,24 @@ public class Class01 {
     public Class01 next;
 
 
-    public Class01(int a, String b){
+    public Class01(int a, String b) {
         this.a = a;
         this.b = b;
     }
 
-    public int getA(){
+    public int getA() {
         return this.a;
     }
 
-    public void setA(int a){
+    public void setA(int a) {
         this.a = a;
     }
 
-    public Class01 getNext(){
+    public Class01 getNext() {
         return this.next;
     }
 
-    public void setNext(Class01 next){
+    public void setNext(Class01 next) {
         this.next = next;
     }
 }
